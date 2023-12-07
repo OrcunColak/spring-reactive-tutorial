14b6283220a603f32e5a7136fec8c04c1fabb1fd
Orçun Çolak <orcun.colak@hazelcast.com>
2023-12-07 12:41:39 +0300

    WebFluxTest example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Declarative HTTP Client
 Go to  
 http://localhost:8080/v1/employees
+
+# API Documentation
+Go to  
+http://localhost:8080/api
---
+++ src/test/java/com/colak/springreactivetutorial/controller/EmployeeControllerTest.java
@@ -0,0 +1,53 @@
+package com.colak.springreactivetutorial.controller;
+
+import com.colak.springreactivetutorial.jpa.Employee;
+import com.colak.springreactivetutorial.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(controllers = EmployeeController.class)
+@AutoConfigureWebTestClient
+class EmployeeControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+    @MockBean
+    EmployeeService employeeService;
+
+    @Test
+    void testFindAll() {
+        when(employeeService.findAll())
+                .thenReturn(Flux.just(
+                                new Employee(1, "employee1", "lastname1"),
+                                new Employee(2, "employee2", "lastname2")
+                        )
+                );
+
+        final String url = EmployeeController.EMPLOYEE_URL;
+        webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Employee.class)
+                .hasSize(2)
+                .consumeWith(listEntityExchangeResult -> {
+                    List<Employee> employeeList = listEntityExchangeResult.getResponseBody();
+                    assert employeeList != null;
+                    Employee lastEmployee = employeeList.getLast();
+
+                    assertEquals(2, lastEmployee.getId());
+                });
+    }
+}
